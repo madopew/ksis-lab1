de7f78ec166b5aaac9a0d2f5609aa7c89dbb5b1f
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-02-19 18:07:36 +0300

    Add project files.

---
+++ ksis_lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ksis_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
